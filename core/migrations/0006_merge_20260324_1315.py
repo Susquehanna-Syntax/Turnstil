ba5ee7fcd30e6049a8db2af4c9626fa95ca5ec9c
Connor Haggerty <connorhaggerty9@gmail.com>
2026-03-28 16:21:38 -0400

    -moved inline python tags out of JS script as pycharm threw errors with them   - Deleted the duplicate 0004_event_reg_close_event_reg_open.py   - Updated the 0006 merge migration to drop its dependency on the deleted files

--- core/migrations/0006_merge_20260324_1315.py
+++ core/migrations/0006_merge_20260324_1315.py
@@ -6,7 +6,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("core", "0004_event_reg_close_event_reg_open"),
         ("core", "0005_event_external_link"),
     ]
 
